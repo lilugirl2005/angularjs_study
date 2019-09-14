edb2123efbf45601b437e6366a9c37721b3527a4
lilu <lilugirl2005@126.com>
2019-09-14 20:59:34 +0800

    custom service

--- static/js/service_animate.js
+++ static/js/service_animate.js
@@ -7,15 +7,28 @@
  * @LastEditors: Liuyi
  * @LastEditTime: 2019-09-12 16:29:12
  */
-var app=angular.module('myApp',[]);
 
-app.controller('myController',['$scope','$interval',function($scope,$interval){
-  $scope.seconds=0;
-  var myInterval=$interval(function(){
-    $scope.seconds++;
-  },1000,10,true);
- 
- $scope.$on('$destroy',function(){
-   $scope.cancel(myInterval);
- })
-}]);
+var app = angular.module('myApp', ['ngAnimate']);
+app.controller('myController', function ($scope) {
+  $scope.myImgClass = 'start-class';
+});
+
+app.animation('.fadeOut', function () {
+  return {
+    enter: function (element, parentElement, afterElement, doneCallback) {
+
+    },
+    leave: function (element, doneCallback) {},
+    move: function (element, parentElement, afterElement, doneCallback) {},
+    addClass: function (element, className, done) {
+      jQuery(element).animate({
+        opacity: 0
+      }, 3000);
+    },
+    removeClass: function (element, className, done) {
+      jQuery(element).animate({
+        opacity: 1
+      }, 3000)
+    }
+  }
+});
